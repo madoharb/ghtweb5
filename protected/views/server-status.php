03083a5612d45ca2e972272f1083022d2af05237
ghtweb <adata8gb@gmail.com>
2015-04-09 12:25:04 +0200

    Поменял вывод онлайна для серверов

--- protected/views/server-status.php
+++ protected/views/server-status.php
@@ -1,22 +1,56 @@
+<?php
+/**
+ * @var array $content
+ * @var int $totalOnline
+ */
+
+$maxLimitOnline = 500; // Верхний предел онлайна (чем он ниже тем больше будет закрашена полоска)
+
+// Подключаю библиотеки для работы с графиком онлайна (See: https://github.com/pguso/jquery-plugin-circliful)
+css(assetsUrl() . '/js/libs/circliful/css/jquery.circliful.css');
+js(assetsUrl() . '/js/libs/circliful/js/jquery.circliful.min.js', CClientScript::POS_END);
+
+clientScript()->registerScript('circliful', '
+
+    $(function(){
+        $(".circuit").circliful();
+    });
+
+', CClientScript::POS_END);
+?>
+
 <?php if(config('server_status.allow')) { ?>
     <?php if($content) { ?>
-        <ul>
-            <?php foreach($content as $gsId => $row) { ?>
-                <li class="<?php echo $row['gs_status'] ?>">
+        <table>
+            <tr>
+                <?php foreach($content as $gsId => $row) { ?>
                     <?php if(isset($row['error'])) { ?>
-                        <?php echo $row['error'] ?>
+                        <td><?php echo $row['error'] ?></td>
                     <?php } else { ?>
-                        <span class="count"><?php echo $row['online'] ?></span>
-                        <span class="name">
-                            <?php echo CHtml::link($row['gs']->name, array('/stats/default/index', 'gs_id' => $row['gs']->id)) ?>
-                        </span>
+                        <?php
+                        /**
+                         * Кол-во игроков: $row['online']
+                         * Название сервера: $row['gs']->name
+                         * Ссылка на статистику сервера: url('/stats/default/index', array('gs_id' => $row['gs']->id))
+                         */
+                        ?>
+                        <td>
+                            <div class="circuit"
+                                 data-dimension="130"
+                                 data-info="<?php echo $row['online'] ?>"
+                                 data-width="10"
+                                 data-text="<?php echo $row['gs']->name ?>"
+                                 data-fgcolor="#CEB004"
+                                 data-bgcolor="#251510"
+                                 data-fill="#AD3E23"
+                                 data-total="<?php echo $maxLimitOnline ?>"
+                                 data-part="<?php echo $row['online'] ?>"
+                                ></div>
+                        </td>
                     <?php } ?>
-                </li>
-            <?php } ?>
-            <?php if(count($content) > 1) { ?>
-                <?php echo $totalOnline ?>
-            <?php } ?>
-        </ul>
+                <?php } ?>
+            </tr>
+        </table>
     <?php } else { ?>
         <?php echo Yii::t('main', 'Нет данных.') ?>
     <?php } ?>
