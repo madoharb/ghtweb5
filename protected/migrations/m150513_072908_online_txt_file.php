3aad6b70d204dce290534096ef2cdc6779e8f966
ghtweb <adata8gb@gmail.com>
2015-05-13 10:39:09 +0200

    Сраные табы

--- protected/migrations/m150513_072908_online_txt_file.php
+++ protected/migrations/m150513_072908_online_txt_file.php
@@ -2,11 +2,11 @@
 
 class m150513_072908_online_txt_file extends CDbMigration
 {
-	public function safeUp()
-	{
+    public function safeUp()
+    {
         $order = $this->getDbConnection()
-            ->createCommand("SELECT MAX(`order`) FROM {{config_group}}")
-            ->queryScalar();
+                      ->createCommand("SELECT MAX(`order`) FROM {{config_group}}")
+                      ->queryScalar();
 
         $this->insert('{{config_group}}', array(
             'name'   => 'Файл online.txt',
@@ -19,37 +19,37 @@ class m150513_072908_online_txt_file extends CDbMigration
         $order = 0;
 
         $this->insert('{{config}}', array(
-            'param'         => 'online.txt.allow',
-            'value'         => 1,
-            'default'       => 1,
-            'label'         => 'Вкл. генерацию файла online.txt',
-            'group_id'      => $groupId,
-            'order'         => ++$order,
-            'field_type'    => 'dropDownList',
-            'created_at'    => date('Y-m-d H:i:s'),
+            'param'      => 'online.txt.allow',
+            'value'      => 1,
+            'default'    => 1,
+            'label'      => 'Вкл. генерацию файла online.txt',
+            'group_id'   => $groupId,
+            'order'      => ++$order,
+            'field_type' => 'dropDownList',
+            'created_at' => date('Y-m-d H:i:s'),
         ));
 
         $this->insert('{{config}}', array(
-            'param'         => 'online.txt.time_refresh',
-            'value'         => 60,
-            'default'       => 60,
-            'label'         => 'Время через сколько обновятся данные в файле online.txt (в минутах)',
-            'group_id'      => $groupId,
-            'order'         => ++$order,
-            'field_type'    => 'textField',
-            'created_at'    => date('Y-m-d H:i:s'),
+            'param'      => 'online.txt.time_refresh',
+            'value'      => 60,
+            'default'    => 60,
+            'label'      => 'Время через сколько обновятся данные в файле online.txt (в минутах)',
+            'group_id'   => $groupId,
+            'order'      => ++$order,
+            'field_type' => 'textField',
+            'created_at' => date('Y-m-d H:i:s'),
         ));
 
         $this->addColumn('{{gs}}', 'online_txt_allow', 'tinyint(1) unsigned DEFAULT 1 not null');
-	}
+    }
 
-	public function safeDown()
-	{
+    public function safeDown()
+    {
         $this->delete('{{config_group}}', 'name = "Файл online.txt"');
 
         $this->delete('{{config}}', 'param = "online.txt.allow"');
         $this->delete('{{config}}', 'param = "online.txt.time_refresh"');
 
         $this->dropColumn('{{gs}}', 'online_txt_allow');
-	}
+    }
 }
